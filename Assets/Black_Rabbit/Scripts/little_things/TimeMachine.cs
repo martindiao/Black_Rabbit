de42535c74b970dcd98136f3077c1d679b81bd18
FungusFlowchart <542461670@qq.com>
2019-06-17 07:29:23 +0800

    little functions

---
+++ Assets/Black_Rabbit/Scripts/little_things/TimeMachine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeMachine : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Black_Rabbit/Scripts/little_things/game_base.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_base : MonoBehaviour
+{
+
+    public void LoadScene(string scene)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

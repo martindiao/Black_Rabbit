7e2f50037402759e380b39b2c143b5410a81e29d
FungusFlowchart <542461670@qq.com>
2019-06-16 21:46:18 +0800

    add a script

---
+++ Assets/Black_Rabbit/Scripts/Camera/SimpleCameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Black_Rabbit
+{
+    public class SimpleCameManager : MonoBehaviour
+    {
+
+        public GameObject[] Cams;
+
+        public void LookAt(int index)
+        {
+            CloseAll();
+            Cams[index].SetActive(true);
+        }
+
+        public void CloseAll()
+        {
+            foreach (GameObject cam in Cams)
+            {
+                cam.SetActive(false);
+            }
+        }
+        void Start()
+        {
+            CloseAll();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+

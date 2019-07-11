169b4baabd11ceec4a5117401c6624e484e1c709
FungusFlowchart <542461670@qq.com>
2019-07-11 23:03:56 +0800

    add some little functions

---
+++ Assets/Black_Rabbit/Scripts/UI/Hover_Butom_Change_Image.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Hover_Butom_Change_Image : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public bool isThisUseful=true;
+    public Sprite Toshow;
+    public Image show;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+        if (isThisUseful)
+        {
+            show.gameObject.SetActive(true);
+            print("In it " + gameObject.name);
+            show.sprite = Toshow;
+        }
+        
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (isThisUseful)
+        {
+            print("Out it " + gameObject.name);
+            show.gameObject.SetActive(false);
+        }
+        //throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Black_Rabbit/Scripts/little_things/decide_isshowOnstart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Black_Rabbit
+{
+    public class decide_isshowOnstart : MonoBehaviour
+    {
+        public bool isShowOnStart = true;
+        // Start is called before the first frame update
+        void Start()
+        {
+            gameObject.SetActive(isShowOnStart);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
